9b89fb43e1b9169a6e96af25f2b8d2b2ca36a21b
Ale i <allleizq@gmail.com>
2022-07-20 18:21:32 -0700

    added basic html structure to generatehtml file

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -6,30 +6,40 @@
 
 function generateHtml(data) {
     return ` 
-    <head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"></head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/style.css" >
+    <title>Profile Generator</title>
+    </head>
+
+    <body>
     <header>My Team</header>
-    <h2>${data.managername}</h2> 
-    <h3>ID: ${data.managerid}</h3>
-    <h3>Email: ${data.manageremail}</h3>
-    <h3>Office number: ${data.manageroffice}</h3>
+    <h2>${data.managerName}</h2> 
+    <h3>ID: ${data.managerId}</h3>
+    <h3>Email: ${data.managerEmail}</h3>
+    <h3>Office number: ${data.managerOffice}</h3>
+
 
     
+    <h2>${data.engineerName}</h2> 
+    <h3>ID: ${data.engineerId}</h3>
+    <h3>Email: ${data.engineerEmail}</h3>
+    <h3>Office number: ${data.engineerGithub}</h3>
+
+
+    <h2>${data.internName}</h2> 
+    <h3>ID: ${data.internId}</h3>
+    <h3>Email: ${data.internEmail}</h3>
+    <h3>Office number: ${data.internSchool}</h3>
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    </body>
+    </html>
     `;
 }
 
