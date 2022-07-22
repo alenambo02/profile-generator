2ed5028a0c53f8b2500af15ef121568543d4bec5
Ale i <allleizq@gmail.com>
2022-07-21 23:48:09 -0700

    fixed html helper and created readme and added pictures for read me

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# profile-generator
+# Profile Generator 
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  #Table of Content
+  -[description](#Description)
+  -[installation](#Installation)
+  -[usage](#Usage)
+  -[credits](#Credits)
+  -[license](#License)
+  -[contact](#Contact)
+
+  ## Description:
+  The purpose behind this project was to create a profile generator that will allow you to add as many team members as you wish. This generator allows you to choose from a manager, engineer, and intern. Each profile includes the team members name, id, email, and role. Now developing a team page can be done with ease. 
+
+  You can see the profile generator layout below:
+
+   ![alt text](./assets/profile%20image.png)
+
+  ## Installation:
+  In order to be able to utilize this generator within your command line, you would have to install node.js, npm inquirer package, and jest. Npm inquirer package allows for prompts to be displayed to you within the command line. For this project, I utilized jest to test my code, therefore if I later make changes to my code I can do it with ease. I will be able to see were my code is breaking and have a better understanding of what to fix. 
+
+  Below, I have displayed how I utilized inquirer to prompt questions within the command line:
+
+   ![alt text](./assets/prompts%20for%20profile.png)
+
+  Below, I have displayed how I utilized jest to test my code within the command line:
+
+  ![alt text](./assets/test.png)
+
+
+  ## Usage:
+  You will be prompted with questions inside the command line to help you generate your team. You can either answer the question or leave it blank to move on to the next question. Your answers to the questions will then be generated below the correct section of employee type you choose to create. Once you are done adding team members to your template a html file will be generated for you. Once this is opened on the browser you will be displayed with cards of each team member you added. This design was done utilizing bootstrap. 
+  
+  Here you can see how I have used 
+  ```
+  fs.writeFile
+  ```
+  
+  ![alt text](./assets/function.png)
+
+
+
+  ## Credits:
+  Helpful video: https://www.youtube.com/watch?v=Jv2uxzhPFl4&t=565s
+  Documentation needed for jest: https://jestjs.io/docs/getting-started
+  I utilized https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba to generate markdown license badges.
+  Contribution: Priya Rizal
+
+ 
+  ## License:
+  MIT 
+
+  ## Contact:
+  allleizq@gmail.com
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,65 +1,118 @@
-// function changeData(data) {
-//     console.log(data)
-    
-//     console.log(engineer)
-// }
-
-
-
-
-
-
 function generateHtml(data) {
-    const engineer = data.filter((engineer)=> {return engineer.getRole() === "Engineer"})
-    const intern = data.filter((intern)=> {return intern.getRole() === "Intern"})
-    
-    return ` 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/style.css">
-    <title>Profile Generator</title>
-    </head>
-    
-    <body>
-    <header id ="head" >My Team</header>
-    
-    <div class="row">
-        <div class= "col card shadow-lg p-3 mb-5 bg-body rounded">
-        <h3>Name: ${data[0].getName()}</h3>
-        <h2>Role: ${data[0].getRole()}</h2> 
-        <h3>ID: ${data[0].getId()}</h3>
-        <h3>Email: ${data[0].getEmail()}</h3>
-        <h3>Office number: ${data[0].getOfficeNumber()}</h3>
-        </div>
-    
-        <div class= "col card shadow-lg p-3 mb-5 bg-body rounded">
-        ${engineer.map((i) => {return `
-        <h2>Name: ${i.getName()}</h2> 
-        <h2>Role: ${i.getRole()}</h2> 
-        <h3>ID: ${i.getId()}</h3>
-        <h3>Email: ${i.getEmail()}</h3>
-        <h3>Github: ${i.getGithub()}</h3>
-        `})} </div>
+    const engineer = data.filter((engineer) => { return engineer.getRole() === "Engineer" })
+    const intern = data.filter((intern) => { return intern.getRole() === "Intern" })
 
-        <div class= "col card shadow-lg p-3 mb-5 bg-body rounded">
-        ${intern.map((i) => {return `
-        <h2>Name: ${i.getName()}</h2> 
-        <h2>Role: ${i.getRole()}</h2> 
-        <h3>ID: ${i.getId()}</h3>
-        <h3>Email: ${i.getEmail()}</h3>
-        <h3>School: ${i.getSchool()}</h3>
-        `})} </div>
-    </div>
-    
-    </body>
-    </html>
-    `;
+    return ` 
+ 
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <title>My Team</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                <link rel="stylesheet" href="style.css">
+                    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+                </head>
+                <body>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12 jumbotron mb-3 team-heading bg-success">
+                                <h1 class="text-center font-weight-bold"">THE TEAM</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="team-area col-12 d-flex justify-content-center">
+                                ${generateTeam(data)}
+                            </div>
+                        </div>
+                    </div>
+                </body>
+            </html>
+            `;
 }
+
+
+
+function generateTeam(data) {
+    const pageHTML = [];
+    pageHTML.push(data
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager)));
+
+    pageHTML.push(data
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer)));
+
+    pageHTML.push(data
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern)));
+
+
+    return pageHTML.join("");
+}
+
+const generateManager = manager => {
+    return `
+    <div class="card bg-secondary m-4">
+    <div class="card-header bg-info">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+    `;
+};
+const generateEngineer = engineer => {
+    return `
+    <div class="card bg-secondary m-4">
+    <div class="card-header bg-info">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">Github: <a target= "_blank" href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+        </ul>
+    </div>
+</div>
+    `;
+};
+const generateIntern = intern => {
+    return `
+    <div class="card bg-secondary m-4">
+    <div class="card-header bg-info">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School:<a target= "_blank" href ="https://www.${intern.getSchool()}.edu">${intern.getSchool()}</a></li>
+        </ul>
+    </div>
+</div>
+    `;
+};
+
+
+
+
+
+
 
 
 
